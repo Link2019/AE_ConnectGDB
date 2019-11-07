f441e914c104a2d22af617826d03fda0ee9d5263
Administrator <Administrator@USER-20190915QG>
2019-11-07 20:55:15 +0800

    获取所有要素类

--- AE_ConnectGDB/AE_ConnectGDB/Form1.cs
+++ AE_ConnectGDB/AE_ConnectGDB/Form1.cs
@@ -23,9 +23,90 @@ namespace AE_ConnectGDB
 
         private void 读取数据ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            AddSDELayer();
-        }
+            //获取单个要素
+            //AddSDELayer();
+            //定义一个数据库连接属性
+            IPropertySet propset = new PropertySetClass();
+            //采用SDE连接
+            //社渚数据库服务器名, 服务器所在的名称(即实例)IP地址？？
+            propset.SetProperty("SERVER", @"USER-20190915QG\SQLEXPRESS");
+            //设置SDE的端口,这是安装时指定的, 默认安装时"port:5151"??
+            propset.SetProperty("INSTANCE", @"sde:sqlserver:USER-20190915QG\SQLEXPRESS");
+            //SDE的用户名
+            propset.SetProperty("USER", "sa");
+            //密码
+            propset.SetProperty("PASSWORD", "root");
+            //设置数据库的名字，只有SQL Server Informix数据库才需要设置
+            propset.SetProperty("DATABASE", "sde");
+            //SDE的版本, 这里为默认版本
+            propset.SetProperty("VERSION", "sde.Default");
 
+            //定义一个工作空间, 并实例化为SDEd工作空间
+            IWorkspaceFactory workspaceFactroy = new SdeWorkspaceFactoryClass();
+            //打开SDE工作空间，并强转为要素工作空间
+            IWorkspace workspace = workspaceFactroy.Open(propset, 0);
+            List<string> listFeaClass=GetFeatureClassByWorkspace(workspace);
+            int i = 0;
+            foreach (var item in listFeaClass)
+            {
+                IFeatureWorkspace featureWorkspace = workspace as IFeatureWorkspace;
+                IFeatureClass featureClass = featureWorkspace.OpenFeatureClass(item);
+                IFeatureLayer featureLayer = new FeatureLayer();
+                featureLayer.FeatureClass = featureClass;
+                featureLayer.Name = featureClass.AliasName;
+                axMapControl1.AddLayer(featureLayer, i);
+                i++;
+            }
+            axMapControl1.Refresh();
+
+        }
+        /// <summary>
+        /// 获取工作空间的所有要素类
+        /// </summary>
+        /// <param name="workspace"></param>
+        /// <param name="workspaceType"></param>
+        /// <returns></returns>
+        public List<string> GetFeatureClassByWorkspace(IWorkspace workspace)
+        {
+            List<string> listFeaClass = new List<string>();
+            try
+            {
+                //遍历工作空间下的featureclass
+                IFeatureWorkspace pFeatureWorkspace = workspace as IFeatureWorkspace;
+                IEnumDataset pEnumDatasets = workspace.get_Datasets(esriDatasetType.esriDTAny) as IEnumDataset;
+                IDataset pDataset = pEnumDatasets.Next();
+                while (pDataset != null)
+                {
+                    if (pDataset.Type == esriDatasetType.esriDTFeatureClass)
+                    {
+                        //string aliasName = pFeatureWorkspace.OpenFeatureClass(pDataset.Name).AliasName;
+                        listFeaClass.Add(pDataset.Name);
+                    }
+                    else if (pDataset.Type == esriDatasetType.esriDTFeatureDataset)
+                    {
+                        IEnumDataset pESubDataset = pDataset.Subsets;
+                        IDataset pSubDataset = pESubDataset.Next();
+                        while (pSubDataset != null)
+                        {
+                            listFeaClass.Add(pSubDataset.Name);
+
+                            pSubDataset = pESubDataset.Next();
+                        }
+                    }
+
+                    pDataset = pEnumDatasets.Next();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return listFeaClass;
+        }
+        /// <summary>
+        /// 获取单个要素
+        /// </summary>
         private void AddSDELayer()
         {
             //定义一个数据库连接属性
@@ -55,5 +136,6 @@ namespace AE_ConnectGDB
             axMapControl1.AddLayer(featureLayer);
             axMapControl1.Refresh();
         }
+
     }
 }
